de6719436be873115c4a7a5d1d4d52df36e28dbe
kazz <gang9696@gmail.com>
2023-07-04 17:54:14 +0900

    Sort plus

--- main.cpp
+++ main.cpp
@@ -106,11 +106,112 @@ void BubbleSort(int input[], int size)
 }
 
 //Insertion Sort
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
 void InsertionSort(int input[], int size)
 {
-	for (int i = 0; i < size; i++) {
-		for(int j=0; j)
-   }
+	for (int i = 1; i < size; i++) 
+	{
+		int j = i;
+		int target = input[i];
+
+		while (--j >= 0 && target < input[j])
+		{
+			input[j + 1] = input[j];
+			input[j] = target;
+		}
+    }
+}
+// Merge Sort
+// Time Complexity: O(n * log n)
+// Space Complexity: O(n)
+void Merge(int input[], int start, int half, int end, int temp[])
+{
+	int i{ start };
+	int j{ half + 1 };
+	int tempIndex{};
+
+	while (i <= half && j <= end)
+	{
+		if (input[i] < input[j])
+		{
+			temp[tempIndex++] = input[i++];
+		}
+		else
+		{
+			temp[tempIndex++] = input[j++];
+		}
+	}
+
+	// 남아 있는 것 병합
+	while (i <= half)
+	{
+		temp[tempIndex++] = input[i++];
+	}
+	while (j <= end)
+	{
+		temp[tempIndex++] = input[j++];
+	}
+
+	// 임시 배열 -> 원래배열로 복사
+	tempIndex = 0;
+	for (int i = start; i <= end; i++)
+	{
+		input[i] = temp[tempIndex++];
+	}
+}
+
+void MergeSort(int input[], int start, int end, int temp[])
+{
+	//base casee
+	if (start >= end)
+	{
+		return;
+	}
+	// recursive case 반/반
+
+	int half = (start + end) / 2;
+
+	MergeSort(input, start, half, temp);
+	MergeSort(input, half+1 , end, temp);
+
+	// Merge
+	Merge(input, start, half, end, temp);
+}
+
+// QuickSort - 요즘 프로그래밍에서 가장 많이 사용
+void QuickSort(int input[], int left, int right)
+{
+	int i = left;
+	int j = right;
+	int pivot = input[(left + right) / 2];
+
+	do
+	{
+		while (input[i] < pivot)
+		{
+			i++;
+		}
+		while (input[j] > pivot)
+		{
+			j--;
+		}
+		if (i <= j)
+		{
+			Swap(input[i], input[j]);
+			i++;
+			j--;
+		}
+	} while (i<=j);
+
+	if (left < j)
+	{
+		QuickSort(input, left, j);
+	}
+	if (i < right)
+	{
+		QuickSort(input, i, right);
+	}
 }
 
 int main()
@@ -119,7 +220,15 @@ int main()
 	int array[ARRAY_SIZE]{ 8,7,2,3,1 };
 
 	//SequentialSort(array, ARRAY_SIZE);
-	SelectionSort(array, ARRAY_SIZE);
+	//SelectionSort(array, ARRAY_SIZE);
+	//InsertionSort(array, ARRAY_SIZE);
+
+	/*int tempArray[ARRAY_SIZE]{};
+	MergeSort(array, 0, ARRAY_SIZE - 1,tempArray);
+	*/
+
+	QuickSort(array, 0, ARRAY_SIZE - 1);
+	
 	Print(array, ARRAY_SIZE);
 	
 }
